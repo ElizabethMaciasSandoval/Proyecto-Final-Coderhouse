35554da70d4cb8552ff4581af81d3ccb4ee270b8
ElizabethMaciasSandoval <elizabeth130394@gmail.com>
2022-07-07 14:07:57 -0400

    contenedores

--- src/containers/cartsContainer.js
+++ src/containers/cartsContainer.js
@@ -1,3 +1,4 @@
+const { readFile, writeFile } = require('../fileManager/fileManager')
 // contenedor de carrito
 class ContainerCarts{
 
@@ -7,27 +8,62 @@ class ContainerCarts{
 
   // crear carrito
   async createCart(){
+    this.carts = await readFile('carritos.txt')
+    if(this.carts.length === 0){
+      const cart = {
+        id: 1,
+        timestamp: Date.now,
+        products: []
+      }
+      this.carts.push(cart)
+      await writeFile('carritos.txt', this.carts)
+      return cart
+    }else{
+      const lastIndex = this.carts[this.carts.length - 1].id;
+      const index = lastIndex + 1;
+      const newCart =  {
+        id: index,
+        timestamp: Date.now,
+        products: []
+      }
+      this.carts.push(newCart)
+      await writeFile('carritos.txt', this.carts)
+      return newCart
+    }
 
   }
 
   // eliminar carritopor ID
-  async deleteCartById(){
-
+  async deleteCartById(id){
+    this.carts = await readFile('carritos.txt')
+    const object = this.carts.filter(element => element.id != id)
+    this.carts = object
+    await writeFile('carrito.txt', this.carts)
   }
 
   // listar productos por ID de carrito
-  async listPorductsByCartId(){
-
+  async listPorductsByCartId(id){
+    this.carts = await readFile('carritos.txt')
+    const cart = this.carts.filter(element => element.id === id)
+    return cart
   }
 
   // agregar producto por ID
-  async addProductById(){
+  async addProductById(id, product){
+    this.carts = await readFile('carritos.txt')
+    const index = this.carts.findIndex(element => element.id === id)
+    this.carts[index].product.push(product)
+    await writeFile('carrito.txt', this.carts)
 
   }
 
   // eliminar producto por ID de carrito y por ID de producto
-  async deleteProductById(){
-
+  async deleteProductById(id, idProduct){
+    this.carts = await readFile('carritos.txt')
+    const index = this.carts.findIndex(element => element.id === id);
+    const object = this.carts[index].products.filter(element => element.id != idProduct)
+    this.carts[index].products = object
+    await writeFile('carrito.txt', this.carts)
   }
 } 
 
--- src/containers/productsContainer.js
+++ src/containers/productsContainer.js
@@ -1,3 +1,4 @@
+const { readFile, writeFile } = require('../fileManager/fileManager')
 // contenedor de productos
 class ProductsContainer {
 
@@ -7,27 +8,73 @@ class ProductsContainer {
 
   // obtener todos los productos
   async getAllProducts(){
-
+    this.products = await readFile('productos.txt')
+    console.log(this.products)
+    return this.products
   }
 
   // obtener productos por ID
   async getProductById(id){
-
+    this.products = await readFile('productos.txt')
+    const product = this.products.filter(element => element.id === id);
+    return product
   }
 
   // agregar producto
-  async addProduct(){
-
+  async addProduct(title, description, code, price, thumbnail, stock){
+    this.products = await readFile('productos.txt')
+    if(this.products.length === 0){
+      const product = {
+        title,
+        description,
+        code,
+        price, 
+        thumbnail,
+        timestamp: Date.now,
+        stock,
+        id:1
+      }
+      this.products.push(product)
+      await writeFile('productos.txt', this.products)
+      return product
+    }else{
+      const lastIndex = this.addProduct[this.products.length - 1].id;
+      const index = lastIndex + 1;
+      const newProduct = {
+        title,
+        description,
+        code,
+        price, 
+        thumbnail,
+        timestamp: Date.now,
+        stock,
+        id:index
+      }
+      this.products.push(newProduct)
+      await writeFile('productos.txt', this.products)
+      return newProduct
+    }
   }
 
   // actualizar producto
-  async updateProduct(){
-
+  async updateProduct(id ,title, description, code, price, thumbnail, stock){
+    this.products = await readFile('productos.txt')
+    const index = this.products.findIndex(product => product.id === id)
+    this.products[index].title = title
+    this.products[index].description = description
+    this.products[index].code = code
+    this.products[index].price = price
+    this.products[index].thumbnail = thumbnail
+    this.products[index].timestamp = Date.now
+    this.products[index].stock = stock
   }
 
   // eliminar producto por ID
   async deleteProductById(id){
-
+    this.products = await readFile('productos.txt')
+    const object = this.products.filter(element => element.id != id);
+    this.products = object
+    await writeFile('productos.txt', this.products)
   }
 }
 
